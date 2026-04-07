8ef6d10186e51ca840f4c40055b2ebc1f65a8fc0
ANJANI  BEESU <anjanibeesu@gmail.com>
2026-04-07 20:40:28 +0530

    Update pomodoro.py

--- pomodoro.py
+++ pomodoro.py
@@ -1,13 +1,9 @@
 import time
-
 print("Pomodoro Timer")
-
 print("1. 25 min work / 5 min break")
 print("2. 50 min work / 10 min break")
 print("3. 100 min work / 20 min break")
-
 choice = input("Choose option (1/2/3): ")
-
 if choice == "1":
     work = 25   # TEMP seconds
     brk = 5
@@ -21,23 +17,16 @@ else:
     print("Invalid choice")
     input("Press Enter to exit...")
     exit()
-
 cycles = int(input("How many cycles?: "))
-
 def countdown(timer, label):
     while timer > 0:
         minutes = timer // 60
         seconds = timer % 60
-
         print(f"{label} {minutes:02d}:{seconds:02d}")
-
         user = input("Press Enter to continue or 'p' to pause: ")
-
         if user.lower() == 'p':
             input("Paused. Press Enter to resume...")
-
         timer -= 1
-
 for i in range(1, cycles + 1):
     print(f"\nCycle {i} - Work")
     countdown(work, "Work")
